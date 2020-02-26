cfa1e7c32c01039956be74558a27c888cf0365c7
Karn Kaul <karnkaul@gmail.com>
2020-02-25 22:26:50 -0800

    Edit travis CMake input variables
    - Rename `LE3D_UPDATE_SUBMODULES` to `GIT_UPDATE_SUBMODULES`.
    - Log if `CI_BUILD`.

--- .travis/script.sh
+++ .travis/script.sh
@@ -11,11 +11,12 @@ cmake --version;
 echo -e "\nninja: $(ninja --version)\n"
 
 # Build cpp-template
-build() {
+build() 
+{
 	CONFIG=$1
 	echo "== Building $PROJECT_NAME | $CONFIG..."
 	[[ ! -d out/$CONFIG ]] && mkdir -p out/$CONFIG
-	cmake -G Ninja $2 -B $3/$CONFIG -DCMAKE_BUILD_TYPE=$CONFIG -DLE3D_UPDATE_SUBMODULES=0 -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_C_COMPILER=clang -DCI_BUILD=1
+	cmake -G Ninja $2 -B $3/$CONFIG -DCMAKE_BUILD_TYPE=$CONFIG -DGIT_UPDATE_SUBMODULES=0 -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_C_COMPILER=clang -DCI_BUILD=1
 	ninja -v -C out/$CONFIG $BUILD_FLAGS
 }
 
