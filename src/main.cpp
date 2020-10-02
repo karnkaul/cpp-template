f7a3f3c50462dd4f8a02212840f2e07718f0f22e
Karn Kaul <karnkaul@gmail.com>
2020-10-01 22:20:16 -0700

    Update clang-format: use `Attach`

--- src/main.cpp
+++ src/main.cpp
@@ -1,57 +1,44 @@
 #include <filesystem>
 #include <iostream>
-#include <string>
 #include <sstream>
+#include <string>
 
-namespace
-{
-void format(std::ostream& out_str, std::string_view fmt)
-{
+namespace {
+void format(std::ostream& out_str, std::string_view fmt) {
 	out_str << fmt;
 }
 
 template <typename Arg0, typename... Args>
-void format(std::ostream& out_str, std::string_view fmt, Arg0&& arg0, Args&&... args)
-{
-	if (!fmt.empty())
-	{
+void format(std::ostream& out_str, std::string_view fmt, Arg0&& arg0, Args&&... args) {
+	if (!fmt.empty()) {
 		constexpr static std::string_view token = "{}";
 		auto const search = fmt.find(token);
-		if (search < fmt.size())
-		{
+		if (search < fmt.size()) {
 			auto const lhs = fmt.substr(0, search);
 			auto const rhs = fmt.substr(search + token.size());
 			out_str << lhs << std::forward<Arg0>(arg0);
 			format(out_str, rhs, std::forward<Args>(args)...);
-		}
-		else
-		{
+		} else {
 			format(out_str, fmt);
 		}
 	}
 }
 
 template <typename... Args>
-std::string format(std::string_view fmt, Args&&... args)
-{
+std::string format(std::string_view fmt, Args&&... args) {
 	std::stringstream str;
 	format(str, fmt, std::forward<Args>(args)...);
 	return str.str();
 }
 } // namespace
 
-int main(int argc, char** argv)
-{
-	if (argc > 0)
-	{
+int main(int argc, char** argv) {
+	if (argc > 0) {
 		std::string_view const argv0 = argv[0];
 		auto const full_path = std::filesystem::absolute(argv0);
-		if (std::filesystem::is_regular_file(full_path))
-		{
+		if (std::filesystem::is_regular_file(full_path)) {
 			std::cout << format("Executable located at: {}\n", full_path.generic_string());
-		}
-		else
-		{
+		} else {
 			std::cerr << "Executable not found!\n";
 		}
 	}
